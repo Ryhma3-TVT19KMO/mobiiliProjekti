7b498f2a5f9c76b17a22dcd8497aeca11ea402be
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-11-05 19:49:13 +0200

    Login and sign up started

--- app/src/main/java/projekti/mobiili/LogIn.java
+++ app/src/main/java/projekti/mobiili/LogIn.java
@@ -2,13 +2,21 @@ package projekti.mobiili;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in_activity);
+
+
+        });
     }
 }
--- app/src/main/java/projekti/mobiili/SignUp.java
+++ app/src/main/java/projekti/mobiili/SignUp.java
@@ -2,13 +2,19 @@ package projekti.mobiili;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_in_activity);
+        setContentView(R.layout.sign_up_activity);
+
     }
 }

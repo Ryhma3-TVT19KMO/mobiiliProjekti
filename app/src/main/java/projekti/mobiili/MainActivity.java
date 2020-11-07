5aea292ef72937d6534d9ade55b848831a3c77ff
Marianne <t8kama06@students.oamk.fi>
2020-11-07 14:11:00 +0200

    Menu added

--- app/src/main/java/projekti/mobiili/MainActivity.java
+++ app/src/main/java/projekti/mobiili/MainActivity.java
@@ -2,13 +2,47 @@ package projekti.mobiili;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button menuNappi, karttaNappi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuNappi = (Button) findViewById(R.id.menuNappi);
+        karttaNappi = (Button) findViewById(R.id.karttaNappi);
+
+        menuNappi.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                openMenu();
+            }
+        });
+
+        karttaNappi.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
+    }
+
+    public void openMenu(){
+        Intent intent = new Intent(this, Ruokalista.class);
+            startActivity(intent);
+    }
+
+    public void openMap(){
+        Intent intent = new Intent(this, Ruokalista.class);
+        startActivity(intent);
     }
 }
